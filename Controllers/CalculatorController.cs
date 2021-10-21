c2866a3e3eddb47c5cce916df8c36c47e894ecd6
Betel Fantahun <bfantahu@students.kennesaw.edu>
2021-10-21 15:40:15 -0400

    all the files need to run the program committed

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetWorthCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetWorthCalculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(NetworthModel n)
+        {
+            // If all the values of the model are filled the networth will be calculated.
+            if (ModelState.IsValid)
+            {
+                n.Total_cash = n.Checkings_accounts + n.Savings_accounts + n.Money_markets_accounts + n.Savings_bonds + n.Cd + n.Other;
+                n.total_liabilities = n.Credit_card_balance + n.Estimated_income_taxowed + n.Other_outstanding_bills + n.Home_mortgage +
+                    n.Home_equity_loans + n.Mortgage_on_renatal_properties + n.Car_loans + n.Student_loans + n.Life_insurance_policy_loans +
+                    n.Other_longterm_debts;
+                n.netWorth = n.Total_cash + n.total_liabilities;
+                @ViewData["cash"] = n.Total_cash;
+                ViewData["liabilities"] = n.total_liabilities;
+                ViewData["net"] = n.netWorth;
+                return View();
+            }
+            // If all the required fields are not entered user will be redirected to the calculator page
+            else
+            {
+                return Redirect("/Calculator");
+            }
+        }
+
+    }
+}
---
+++ Models/NetworthModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetWorthCalculator.Models
+{
+    public class NetworthModel
+    {
+        // properties for the assets col
+        [Required]
+        public double Checkings_accounts { get; set; }
+        [Required]
+        public double Savings_accounts { get; set; }
+        [Required]
+        public double Money_markets_accounts { get; set; }
+        [Required]
+        public double Savings_bonds { get; set; }
+        [Required]
+        public double Cd { get; set; }
+        [Required]
+        public double Other { get; set; }
+
+        public double Total_cash { get; set; }
+
+
+
+        //fields for the liabilities
+        [Required]
+        public double Credit_card_balance { get; set; }
+        [Required]
+        public double Estimated_income_taxowed { get; set; }
+        [Required]
+        public double Other_outstanding_bills { get; set; }
+        [Required]
+        public double Home_mortgage { get; set; }
+        [Required]
+        public double Home_equity_loans { get; set; }
+        [Required]
+        public double Mortgage_on_renatal_properties { get; set; }
+        [Required]
+        public double Car_loans { get; set; }
+        [Required]
+        public double Student_loans { get; set; }
+        [Required]
+        public double Life_insurance_policy_loans { get; set; }
+        [Required]
+        public double Other_longterm_debts { get; set; }
+        
+        public double total_liabilities { get; set; }
+        
+        public double netWorth { get; set; }
+        public string calculate { get; set; }
+
+    }
+}
